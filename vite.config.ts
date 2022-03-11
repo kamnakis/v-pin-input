fb6df7a25d20af7f76dc9b7b791766b1058b62a1
Stelios Kamnakis <skamnakis@lancom.gr>
2022-03-11 23:13:35 +0200

    name changes

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
-      name: "SplitInput",
+      name: "PinInput",
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
