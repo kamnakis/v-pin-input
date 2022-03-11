8bc4c5aaf72f487f02bde8a1a5a7735e9c416802
Stelios Kamnakis <skamnakis@lancom.gr>
2022-03-11 23:49:43 +0200

    config

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { App } from "vue"
 import PinInputVue from "./components/PinInput.vue"
 
+export type { Cell, CellInputType, CellsInputTypes } from './types/types'
+
 const PinInput = {
   install: (app: App, options = {}): void => {
     app.component("pin-input", PinInputVue)
